4bfa0d9835ea78c1d994d04d908cc63c047722b0
mooner1022 <moonmk10221@gmail.com>
2021-12-16 00:24:07 +0900

    Add NotificationUtils.kt

---
+++ app/src/main/java/com/mooner/starlight/utils/NotificationUtils.kt
@@ -0,0 +1,25 @@
+package com.mooner.starlight.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+fun Context.createNotificationChannel(channelId: String, channelName: String, channelDescription: String? = null, importance: Int = NotificationManager.IMPORTANCE_DEFAULT) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationChannel = NotificationChannel(
+            channelId,
+            channelName,
+            importance
+        )
+        //notificationChannel.enableLights(false)
+        //notificationChannel.enableVibration(false)
+        if (channelDescription != null)
+            notificationChannel.description = channelDescription
+
+        val notificationManager = applicationContext.getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            notificationChannel)
+    }
+}
