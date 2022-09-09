64d75e03427808673d9ee1035af37193e8c68b0a
mooner1022 <moonmk10221@gmail.com>
2022-09-09 23:15:32 +0900

    Add experimental Sheets library dependency

--- app/src/main/java/dev/mooner/starlight/utils/DialogUtils.kt
+++ app/src/main/java/dev/mooner/starlight/utils/DialogUtils.kt
@@ -1,17 +1,25 @@
 package dev.mooner.starlight.utils
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.afollestad.materialdialogs.LayoutMode
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.bottomsheets.BottomSheet
-import com.afollestad.materialdialogs.callbacks.onDismiss
-import com.afollestad.materialdialogs.customview.customView
-import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
+import com.afollestad.materialdialogs.bottomsheets.setPeekHeight
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.maxkeppeler.sheets.color.ColorSheet
+import com.maxkeppeler.sheets.core.Sheet
+import com.maxkeppeler.sheets.core.SheetStyle
+import com.maxkeppeler.sheets.info.InfoSheet
 import dev.mooner.starlight.R
+import dev.mooner.starlight.databinding.DialogLogsBinding
 import dev.mooner.starlight.plugincore.Session
 import dev.mooner.starlight.plugincore.event.EventHandler
 import dev.mooner.starlight.plugincore.event.Events
@@ -20,12 +28,89 @@ import dev.mooner.starlight.plugincore.logger.LogType
 import dev.mooner.starlight.plugincore.logger.Logger
 import dev.mooner.starlight.ui.logs.LogsRecyclerViewAdapter
 import jp.wasabeef.recyclerview.animators.FadeInUpAnimator
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-fun Context.showLogsDialog(): MaterialDialog {
+class CustomSheet: Sheet() {
+
+    private lateinit var binding: DialogLogsBinding
+    private val logUpdateJob = Job()
+
+    override fun onCreateLayoutView(): View =
+        DialogLogsBinding.inflate(LayoutInflater.from(activity)).also { binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        style(SheetStyle.BOTTOM_SHEET)
+        displayHandle(true)
+        displayCloseButton(true)
+        displayPositiveButton(false)
+        width = resources.getDimensionPixelSize(R.dimen.dialog_width)
+
+        val activity = requireActivity()
+
+        val logs = Logger.logs
+        val mAdapter = LogsRecyclerViewAdapter(activity).apply {
+            data = logs.toMutableList()
+        }
+        val mLayoutManager = LinearLayoutManager(activity).apply {
+            reverseLayout = true
+            stackFromEnd = true
+        }
+
+        binding.rvLog.apply {
+            itemAnimator = FadeInUpAnimator()
+            layoutManager = mLayoutManager
+            adapter = mAdapter
+        }
+        mAdapter.notifyItemRangeInserted(0, logs.size)
+
+        CoroutineScope(Dispatchers.Main + logUpdateJob).launch {
+            EventHandler.on<Events.Log.LogCreateEvent>(this) {
+                if (log.type == LogType.VERBOSE && !Session.globalConfig.category("dev_mode_config").getBoolean("show_internal_log", false)) return@on
+                mAdapter.pushLog(log)
+                binding.rvLog.let { recycler ->
+                    recycler.post {
+                        recycler.smoothScrollToPosition(mAdapter.data.size - 1)
+                    }
+                }
+            }
+        }
+        super.onViewCreated(view, savedInstanceState)
+        (dialog as BottomSheetDialog?)?.behavior?.apply {
+            setPeekHeight(PEEK_HEIGHT_AUTO, true)
+            isFitToContents = false
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        (dialog as BottomSheetDialog?)?.behavior?.apply {
+            state = STATE_HALF_EXPANDED
+        }
+        return super.onCreateDialog(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logUpdateJob.cancel()
+    }
+
+    fun build(ctx: Context, width: Int? = null, func: CustomSheet.() -> Unit): CustomSheet {
+        this.windowContext = ctx
+        this.width = width
+        this.func()
+        return this
+    }
+
+    fun show(ctx: Context, width: Int? = null, func: CustomSheet.() -> Unit): CustomSheet {
+        this.windowContext = ctx
+        this.width = width
+        this.func()
+        this.show()
+        return this
+    }
+}
+
+fun Context.showLogsDialog() {
+    /*
     val job = Job()
 
     val mLogs = if (Session.globalConfig.category("dev_mode_config").getBoolean("show_internal_log", false))
@@ -73,6 +158,34 @@ fun Context.showLogsDialog(): MaterialDialog {
         onDismiss {
             job.cancel()
         }
+    }
+     */
+
+    /*
+    InfoSheet().show(this) {
+        title("Do you want to install Awake?")
+        peekHeight(dp(400))
+        content("Awake is a beautiful alarm app with morning challenges, advanced alarm management and more.")
+        onNegative("No") {
+            // Handle event
+        }
+        onPositive("Install") {
+            // Handle event
+        }
+    }
+
+    ColorSheet().show(this, width = resources.getDimensionPixelSize(R.dimen.dialog_width)) {
+        //peekHeight(480)
+        peekHeight(dp(400))
+        title("Background color")
+        onPositive { color ->
+            // Use color
+        }
+        displayHandle(true)
+    }
+     */
+    CustomSheet().show(this) {
+
     }
 }
 
