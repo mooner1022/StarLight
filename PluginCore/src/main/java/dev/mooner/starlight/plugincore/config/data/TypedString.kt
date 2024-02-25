c0334a8144674204cdc01393c89d99c2c11fdfcd
mooner1022 <moonmk10221@gmail.com>
2024-02-25 18:05:09 +0900

    Remove unused code

--- PluginCore/src/main/java/dev/mooner/starlight/plugincore/config/data/TypedString.kt
+++ PluginCore/src/main/java/dev/mooner/starlight/plugincore/config/data/TypedString.kt
@@ -9,10 +9,6 @@ package dev.mooner.starlight.plugincore.config.data
 import dev.mooner.starlight.plugincore.Session
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
-import kotlin.reflect.KType
-import kotlin.reflect.typeOf
 
 @Serializable
 sealed interface TypedString <T> {
@@ -22,37 +18,6 @@ sealed interface TypedString <T> {
     fun cast(): T
 
     fun tryCast(): T?
-}
-
-/*
-@Serializable
-class MapTypedString<T: Any>(
-    override val value: String,
-)
- */
-
-@Serializable
-class DynamicTypedString<T: Any>(
-    override val value: String,
-    val type: KType
-): TypedString<T> {
-
-    @Suppress("UNCHECKED_CAST")
-    override fun cast(): T {
-        return Json.decodeFromString(serializer(type), value) as T
-    }
-
-    override fun tryCast(): T? {
-        return runCatching(::cast).getOrNull()
-    }
-
-    companion object {
-        inline operator fun <reified T: Any> invoke(value: String) =
-            DynamicTypedString<T>(value, typeOf<T>())
-
-        inline fun <reified T: Any> from(value: Any): DynamicTypedString<T> =
-            DynamicTypedString(Json.encodeToString(value))
-    }
 }
 
 @Serializable
@@ -105,7 +70,7 @@ data class PrimitiveTypedString(
     companion object {
         private val castable: Array<String> = arrayOf("String", "Boolean", "Float", "Int", "Long", "Double")
 
-        fun isCastable(typeName: String?): Boolean =
+        private fun isCastable(typeName: String?): Boolean =
             typeName in castable
 
         fun from(value: Any): PrimitiveTypedString {
@@ -119,12 +84,5 @@ data class PrimitiveTypedString(
     }
 }
 
-inline fun <reified T: Any> String.typedAs(): TypedString<T> =
-    DynamicTypedString(this)
-
 infix fun String.typedAs(type: String): PrimitiveTypedString =
     PrimitiveTypedString(type, this)
-
-@Suppress("UNCHECKED_CAST")
-fun <T, V> TypedString<T>.castAs(): V =
-    this.cast() as V
